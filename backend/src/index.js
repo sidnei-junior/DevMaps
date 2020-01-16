1acc620796a210e1be3e10c54ab922948b0e9da7
sidneiarauj <sidnei.ifrn@gmail.com>
2020-01-15 22:21:35 -0300

    :construction: Alterações necessárias para acesso com o front-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors')
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-btbk2.mongodb.net/
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
