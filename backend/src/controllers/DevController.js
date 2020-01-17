7b35218770e8796117cc7456bdd198781f09aecd
sidneiarauj <sidnei.ifrn@gmail.com>
2020-01-17 11:09:28 -0300

    :construction: Modificações para conectar com o mobile via websocket

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev')
 const parseStringAsArray = require('../utils/parseStringAsArray')
+const  { findConnections, sendMessage } = require('../websocket');
 
 // index, show, store, update, destroy
 
@@ -35,6 +36,16 @@ module.exports = {
                 techs: techsArray,
                 location,
             });
+
+            // Filtrar as conexões que estão a 10km ou o novo dev 
+            // tenha alguma das tecnologias buscadas
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            )
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
     
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const cors = require('cors')
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-btbk2.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,4 +20,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
